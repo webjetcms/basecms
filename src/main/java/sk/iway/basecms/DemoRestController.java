ddc39d1dd304b665e0b4266e38c91a9ec9ad2e4f
Lubos Balat <lubos.balat@interway.sk>
2023-05-26 13:56:09 +0200

    UPD gradle to support 2023.18+ and Java 17

--- src/main/java/sk/iway/basecms/DemoRestController.java
+++ src/main/java/sk/iway/basecms/DemoRestController.java
@@ -1,5 +1,8 @@
 package sk.iway.basecms;
 
+import java.util.random.RandomGeneratorFactory;
+import java.util.stream.IntStream;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +18,11 @@ public class DemoRestController
 	@GetMapping(path={"/demo-test"})
 	public String test()
 	{
-		return "Demo OK " + Constants.getInstallName();
+		//https://www.baeldung.com/java-17-new-features
+		IntStream random = RandomGeneratorFactory.of("Random")
+				.create()
+				.ints(10, 0, 100);
+
+		return "Demo OK " + Constants.getInstallName() + " " + random.findFirst().getAsInt();
 	}
 }
